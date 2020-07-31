0e3ede90187d35add331965c44a85a88b6142566
guoshengboy <296385487@qq.com>
2020-07-31 10:10:11 +0800

    增加工程

---
+++ GSTool/GSTool/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GSTool
+//
+//  Created by 曹国盛 on 2020/7/31.
+//  Copyright © 2020 cgs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ GSTool/GSToolTests/GSToolTests.m
@@ -0,0 +1,37 @@
+//
+//  GSToolTests.m
+//  GSToolTests
+//
+//  Created by 曹国盛 on 2020/7/31.
+//  Copyright © 2020 cgs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GSToolTests : XCTestCase
+
+@end
+
+@implementation GSToolTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
